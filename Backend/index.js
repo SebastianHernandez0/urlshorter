030071ff6f69e5723928f0e98caff42a1d4e5d83
Sebastián Hernández <sebastian.her0548@gmail.com>
2024-08-01 07:13:09 -0400

    Cambiando cosas para deploy en vercel

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
